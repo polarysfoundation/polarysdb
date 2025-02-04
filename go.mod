d5a5994cdf65c5d766378e892cd292bf30f5d40f
polarysfoundation <polarysfundation@gmail.com>
2025-02-04 19:14:24 -0400

    Add PM-256 dependency and implement key generation functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/polarysfoundation/polarys_db
 
 go 1.23.3
+
+require github.com/polarysfoundation/pm-256 v0.0.0-20250112065549-cb7b6eb92c94
---
+++ keys.go
@@ -0,0 +1,29 @@
+package polarysdb
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+
+	pm256 "github.com/polarysfoundation/pm-256"
+	"github.com/polarysfoundation/polarys_db/modules/common"
+)
+
+func GenerateKey() common.Key {
+	b := make([]byte, 64)
+	_, err := io.ReadFull(rand.Reader, b)
+	if err != nil {
+		fmt.Println("Error generating random bytes:", err)
+		return [32]byte{} // Return an empty array in case of error
+	}
+
+	h := pm256.Sum256(b)
+{}
+	return common.BytesToKey(h[:])
+}
+
+func GenerateKeyFromBytes(b []byte) common.Key {
+	h := pm256.Sum256(b)
+
+	return common.BytesToKey(h[:])
+}
